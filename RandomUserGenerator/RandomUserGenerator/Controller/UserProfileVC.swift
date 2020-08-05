d04673e76da2776e6eaadb0f4bb808784ddf1e19
Dtombacco <tombacco.devron@gmail.com>
2020-08-05 08:21:24 +0100

    Add MVC design pattern

---
+++ RandomUserGenerator/RandomUserGenerator/Controller/UserProfileVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomUserGenerator
+//
+//  Created by Devron Tombacco on 05/08/2020.
+//  Copyright © 2020 dtomswift. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
