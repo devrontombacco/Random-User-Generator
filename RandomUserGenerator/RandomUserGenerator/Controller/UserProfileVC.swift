50134b5bb26d28d0e58260c450cb631eca80a070
Dtombacco <tombacco.devron@gmail.com>
2020-08-05 21:04:35 +0100

    Connect Storyboard UI to UserProfileVC

--- RandomUserGenerator/RandomUserGenerator/Controller/UserProfileVC.swift
+++ RandomUserGenerator/RandomUserGenerator/Controller/UserProfileVC.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class UserProfileVC: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var professionLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var dobLabel: UILabel!
+    @IBOutlet weak var nationalityLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var telephoneNoLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
     }
 
-
+    func configureUI(){
+        configureImage()
+    }
+    
+    func configureImage(){
+        // add cornerRadius to uiimageview here
+    }
+    
+    @IBAction func generateButtonPressed(_ sender: UIButton) {
+        makeRequest()
+    }
+    
+    func makeRequest() {
+        // Code for API call
+    }
 }
 
